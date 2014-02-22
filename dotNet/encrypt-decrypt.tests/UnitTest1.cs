1501f16c31eba25d7f9a6e2dbe129de2ed1424a0
Patricio Sapir <psapir@metricsmarketing.com>
2014-02-22 10:18:58 -0500

    first commit

---
+++ dotNet/encrypt-decrypt.tests/UnitTest1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace encrypt_decrypt.tests
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class UnitTest1
+    {
+         #region Private Members
+        private string DESKEY = "YEP!s8E4";
+
+      
+        private byte[] _DESkey;
+        
+        #endregion
+
+        /// <summary>
+        /// Constructor, very important to have these values converted to bytes in your code!
+        /// </summary>
+        public UnitTest1()
+        {
+        
+            _DESkey = ASCIIEncoding.ASCII.GetBytes(DESKEY);
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void DESEncryption()
+        {
+            Assert.AreEqual(DES.Encrypt("adventures with et rocks!", _DESkey),"N1LboTHKF5hkuJ0oum6AycE8tohUknyTMiuw1fbrJ1s=");
+        }
+
+        [TestMethod]
+        public void DESDecryption()
+        {
+            Assert.AreEqual(DES.Decrypt("N1LboTHKF5hkuJ0oum6AycE8tohUknyTMiuw1fbrJ1s=", _DESkey), "adventures with et rocks!");
+        }
+
+    }
+}
---
+++ dotNet/encrypt-decrypt/DES.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Text;
+using System.Security.Cryptography;
+using System.Linq;
+
+namespace encrypt_decrypt
+{
+    public class DES
+    {
+        public static string Encrypt(string originalString, byte[] key)
+        {
+            try
+            {
+                if (String.IsNullOrEmpty(originalString))
+                {
+                    throw new ArgumentNullException
+                           ("The string which needs to be encrypted can not be null.");
+                }
+                DESCryptoServiceProvider cryptoProvider = new DESCryptoServiceProvider();
+                cryptoProvider.Mode = CipherMode.ECB;
+                cryptoProvider.Padding = PaddingMode.Zeros;
+                MemoryStream memoryStream = new MemoryStream();
+                CryptoStream cryptoStream = new CryptoStream(memoryStream,
+                    cryptoProvider.CreateEncryptor(key, key), CryptoStreamMode.Write);
+                StreamWriter writer = new StreamWriter(cryptoStream);
+                writer.Write(originalString);
+                writer.Flush();
+                cryptoStream.FlushFinalBlock();
+                writer.Flush();
+                return Convert.ToBase64String(memoryStream.GetBuffer(), 0, (int)memoryStream.Length);
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+
+        public static string Decrypt(string cryptedString, byte[] key)
+        {
+            try
+            {
+                if (String.IsNullOrEmpty(cryptedString))
+                {
+                    throw new ArgumentNullException
+                       ("The string which needs to be decrypted can not be null.");
+                }
+                DESCryptoServiceProvider cryptoProvider = new DESCryptoServiceProvider();
+                cryptoProvider.Mode = CipherMode.ECB;
+                cryptoProvider.Padding = PaddingMode.Zeros;
+
+                MemoryStream memoryStream = new MemoryStream
+                        (Convert.FromBase64String(cryptedString));
+                CryptoStream cryptoStream = new CryptoStream(memoryStream,
+                    cryptoProvider.CreateDecryptor(key, key), CryptoStreamMode.Read);
+                StreamReader reader = new StreamReader(cryptoStream);
+                return reader.ReadToEnd().Replace("\0", string.Empty);
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+    }
+}
